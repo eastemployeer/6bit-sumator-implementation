1df2c04981873f56dc4bd0cb5c10e41948c1aa04
Radosław Łodziński <43908048+Radekowicz@users.noreply.github.com>
2020-05-18 20:55:38 +0200

    Add files via upload

---
+++ Generate.java
@@ -0,0 +1,30 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Generate {
+
+    public static void main(String[] args) {
+
+        String data = "";
+
+        for (int i = 0; i <= 4095; i++) {
+            String number = Integer.toBinaryString(i);
+
+            while(number.length() < 12) {
+                number = "0" + number;
+            }
+            data = data + number + "\n";
+        }
+
+        try {
+            Files.write(Paths.get("data.txt"), data.getBytes());
+        }
+        catch ( Exception e) {
+            System.out.println("error");
+        }
+
+
+
+    }
+}
