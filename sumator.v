431c99e158e214311a08ae5d454a4731920fedee
eastemployeer <kamilzab27@gmail.com>
2020-05-19 21:37:46 +0200

    Added result generator, improved files

--- sumator.v
+++ sumator.v
@@ -26,9 +26,9 @@ endmodule
 
 
 
-module adder (a, y, s);
+module adder (x, y, s);
 
-input [5:0] a;
+input [5:0] x;
 input [5:0] y;
 output [6:0] s;
 
@@ -44,12 +44,12 @@ wire [2:0] Ptemp;
 //while(i<8) begin
 /*initial begin
 for (i=0;i<6;i=i+1) begin*/
-single_generate G1 (a[0], y[0], h[0], g[0], p[0]);
-single_generate G2 (a[1], y[1], h[1], g[1], p[1]);
-single_generate G3 (a[2], y[2], h[2], g[2], p[2]);
-single_generate G4 (a[3], y[3], h[3], g[3], p[3]);
-single_generate G5 (a[4], y[4], h[4], g[4], p[4]);
-single_generate G6 (a[5], y[5], h[5], g[5], p[5]);
+single_generate G1 (x[0], y[0], s[0], G[0], P[0]);
+single_generate G2 (x[1], y[1], h[1], g[1], p[1]);
+single_generate G3 (x[2], y[2], h[2], g[2], p[2]);
+single_generate G4 (x[3], y[3], h[3], g[3], p[3]);
+single_generate G5 (x[4], y[4], h[4], g[4], p[4]);
+single_generate G6 (x[5], y[5], h[5], g[5], p[5]);
 /*i=i+1;
   end
 end*/
@@ -57,10 +57,8 @@ end*/
 //single_generate G1 (x[0], y[0], h[0], g[0], p[0]);
 //if (i==0){
 
-assign s[0] = h[0];
-assign G[0] = g[0];
-assign P[0] = p[0];
-
+//KORZYSTAJAC Z FAKTU ZE G[0] = g[0] i P[0] = p[0] i h[0] = s[0] i s[6] = G[5] od razu zastąpiłem
+//
 //}
 //else {
 //POZYCJA 2^1
@@ -70,7 +68,7 @@ final_bit_sum S1 (h[1],G[0],s[1]);
 //end
 
 //POZYCJA 2^2
-propagation P1 (g[1],p[1],g[0],p[0],G[1],P[1]);
+propagation P1 (g[1],p[1],G[0],P[0],G[1],P[1]);
 //single_generate G3
 final_bit_sum S2 (h[2],G[1],s[2]);
 
@@ -89,6 +87,5 @@ final_bit_sum S5 (h[5],G[4],s[5]);
 
 //POZYCJA 2^6
 propagation P5 (g[5],p[5],g[4],p[4],Gtemp[1],Ptemp[1]);
-propagation P5_1 (Gtemp[1],Ptemp[1],G[3],P[3],G[5],P[5]);
-assign s[6] = G[5];
+propagation P5_1 (Gtemp[1],Ptemp[1],G[3],P[3],s[6],P[5]);
 endmodule
