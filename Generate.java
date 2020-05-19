1b697805071d28daffd50333222a68306ad5a1ba
Radosław Łodziński <43908048+Radekowicz@users.noreply.github.com>
2020-05-19 21:20:59 +0200

    Add files via upload

--- Generate.java
+++ Generate.java
@@ -7,14 +7,28 @@ public class Generate {
     public static void main(String[] args) {
 
         String data = "";
+        String number1;
+        String number2;
+        String sum;
 
-        for (int i = 0; i <= 4095; i++) {
-            String number = Integer.toBinaryString(i);
-
-            while(number.length() < 12) {
-                number = "0" + number;
+        for (int i = 0; i <= 63; i++) {
+            number1 = Integer.toBinaryString(i);
+            while(number1.length() < 6) {
+                number1 = "0" + number1;
             }
-            data = data + number + "\n";
+            for (int j = 0; j <= 63; j++) {
+                number2 = Integer.toBinaryString(j);
+                while(number2.length() < 6) {
+                    number2 = "0" + number2;
+                }
+                sum = Integer.toBinaryString(i+j);
+
+                while(sum.length() < 7) {
+                    sum = "0" + sum;
+                }
+                data = data + number1 + number2 + sum + "\n";
+            }
+
         }
 
         try {
