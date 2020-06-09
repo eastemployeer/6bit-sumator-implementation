a49c14deb6eaba9fd383dfe4c1225aa620676206
eastemployeer <50885549+eastemployeer@users.noreply.github.com>
2020-06-09 22:29:21 +0200

    optimization

--- generator.v
+++ generator.v
@@ -13,17 +13,14 @@ initial begin
 
   f = $fopen("data.txt","w+");
 
-  //@(negedge reset); //Wait for reset to be released
-  //@(posedge clk);   //Wait for fisrt clock out of reset
+ 
 
   ii = 0;
   jj = 0;
   ssum = 0;
   for (i=0; i<64; i=i+1) begin
-    //#1; //1 timestamp delay
-    
     for (j=0; j<64; j=j+1) begin
-      //#1; //1 timestamp delay
+     
 
       ssum = ii + jj;
       $fwrite (f, "%b%b%b\n", ii,jj,ssum);
